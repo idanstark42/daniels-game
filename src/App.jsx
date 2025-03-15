ed442c1371906a333af084dbf6d732aaeae6b23d
Idan Stark <idanstark42@gmail.com>
2025-03-15 22:27:53 +0100

    stages

--- src/App.jsx
+++ src/App.jsx
@@ -122,63 +122,63 @@ const threeDigitsLevel = (digits = 523) => ({
 })
 
 const LEVELS = [{},
-  // {
-  //   instructions: 'לחצי על הכפתור הורוד',
-  //   initialState: {},
-  //   content: (_state, _setState, nextLevel) => <button onClick={nextLevel} style={{ background: 'rgb(228, 59, 161)', width: 300, height: 300 }}></button>
-  // }, {
-  //   instructions: 'לחצי על הכפתור הורוד',
-  //   initialState: {},
-  //   content: (_state, _setState, nextLevel) => <div style={{ display: 'flex', gap: 10, alignItems: 'center', justifyContent: 'center' }}>
-  //     <button onClick={nextLevel} style={{ background: 'rgb(228, 59, 161)', width: 300, height: 300 }}></button>
-  //     <button style={{ background: 'rgb(59, 208, 228)', width: 300, height: 300 }}></button>
-  //   </div>
-  // },
-  // smallButtonsLevel(2),
-  // smallButtonsLevel(2),
-  // smallButtonsLevel(6),
-  // smallButtonsLevel(6),
-  // smallButtonsLevel(10),
-  // smallButtonsLevel(20),
-  // smallButtonsLevel(30),
-  // messageLevel('עד עכשיו היה קל? אז בואי ננסה קשה יותר'),
-  // movingButtonLevel(2),
-  // movingButtonLevel(30),
-  // messageLevel('עכשיו ננסה משהו אחר...'),
-  // letterLevel('א'),
-  // letterLevel('ב'),
-  // letterLevel('ג'),
-  // letterLevel('ד'),
-  // letterLevel('ה'),
-  // letterLevel('ו'),
-  // letterLevel('ז'),
-  // letterLevel('ח'),
-  // letterLevel('ט'),
-  // letterLevel('י'),
-  // letterLevel('כ'),
-  // letterLevel('ל'),
-  // letterLevel('מ'),
-  // letterLevel('נ'),
-  // letterLevel('ס'),
-  // letterLevel('ע'),
-  // letterLevel('פ'),
-  // letterLevel('צ'),
-  // letterLevel('ק'),
-  // letterLevel('ר'),
-  // letterLevel('ש'),
-  // letterLevel('ת'),
-  // textLevel('דניאל'),
-  // textLevel('שלום מחשב'),
-  // textLevel('אני כותבת'),
-  // textLevel('קוראים לי דניאל ואני כותבת במחשב'),
-  // {
-  //   instructions: '',
-  //   initialState: {},
-  //   content: (_state, _setState, nextLevel) => <div style={{ width: '100%', height: '80vh', position: 'relative' }}>
-  //     <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', fontSize: 50 }}>סיימת!</div>
-  //   </div>,
-  // }
+  {
+    instructions: 'לחצי על הכפתור הורוד',
+    initialState: {},
+    content: (_state, _setState, nextLevel) => <button onClick={nextLevel} style={{ background: 'rgb(228, 59, 161)', width: 300, height: 300 }}></button>
+  }, {
+    instructions: 'לחצי על הכפתור הורוד',
+    initialState: {},
+    content: (_state, _setState, nextLevel) => <div style={{ display: 'flex', gap: 10, alignItems: 'center', justifyContent: 'center' }}>
+      <button onClick={nextLevel} style={{ background: 'rgb(228, 59, 161)', width: 300, height: 300 }}></button>
+      <button style={{ background: 'rgb(59, 208, 228)', width: 300, height: 300 }}></button>
+    </div>
+  },
+  smallButtonsLevel(2),
+  smallButtonsLevel(2),
+  smallButtonsLevel(6),
+  smallButtonsLevel(6),
+  smallButtonsLevel(10),
+  smallButtonsLevel(20),
+  smallButtonsLevel(30),
+  messageLevel('עד עכשיו היה קל? אז בואי ננסה קשה יותר'),
+  movingButtonLevel(2),
+  movingButtonLevel(30),
+  messageLevel('עכשיו ננסה משהו אחר...'),
+  letterLevel('א'),
+  letterLevel('ב'),
+  letterLevel('ג'),
+  letterLevel('ד'),
+  letterLevel('ה'),
+  letterLevel('ו'),
+  letterLevel('ז'),
+  letterLevel('ח'),
+  letterLevel('ט'),
+  letterLevel('י'),
+  letterLevel('כ'),
+  letterLevel('ל'),
+  letterLevel('מ'),
+  letterLevel('נ'),
+  letterLevel('ס'),
+  letterLevel('ע'),
+  letterLevel('פ'),
+  letterLevel('צ'),
+  letterLevel('ק'),
+  letterLevel('ר'),
+  letterLevel('ש'),
+  letterLevel('ת'),
+  textLevel('דניאל'),
+  textLevel('שלום מחשב'),
+  textLevel('אני כותבת'),
+  textLevel('קוראים לי דניאל ואני כותבת במחשב'),
   threeDigitsLevel(243),
+  {
+    instructions: '',
+    initialState: {},
+    content: (_state, _setState, nextLevel) => <div style={{ width: '100%', height: '80vh', position: 'relative' }}>
+      <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', fontSize: 50 }}>סיימת!</div>
+    </div>,
+  }
 ]
 
 function App() {
